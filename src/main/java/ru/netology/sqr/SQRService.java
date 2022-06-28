f86c15bb80983494d6e9c4606a5f291e4d6b9738
Денис Сергеев <denis.sergeev@vsegda-da.com>
2022-06-29 01:01:27 +0300

    fix by review

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -2,24 +2,24 @@ package ru.netology.sqr;
 
 public class SQRService {
 
-    public int findSquares(int min, int max){
+    public int findSquares(int min, int max) {
 
         int firstDigit = 10;
         int finishDigit = 99;
         int countSquares = 0;
 
-        if (min < firstDigit*firstDigit ||
-            max > finishDigit*finishDigit ){
+        if (min < firstDigit * firstDigit ||
+                max > finishDigit * finishDigit) {
 
             return countSquares;
         }
 
-        for (int i=firstDigit; i<=finishDigit; i++){
+        for (int i = firstDigit; i < finishDigit; i++) {
 
-            int square = i*i;
+            int square = i * i;
 
             if (square >= min &&
-                square <= max){
+                    square < max) {
 
                 countSquares++;
             }
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -5,11 +5,11 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class SQRServiceTestSet {
+public class SQRServiceTest {
 
     @ParameterizedTest
-    @CsvFileSource(files="src/test/resources/testParameters.csv")
-    public void checkCountSquares(int startRange, int endRange, int expectedSqrs){
+    @CsvFileSource(files = "src/test/resources/testParameters.csv")
+    public void checkCountSquares(int startRange, int endRange, int expectedSqrs) {
 
         SQRService sqrService = new SQRService();
 
